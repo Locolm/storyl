c7733c2d3809d518081194e17d7c0bfc88bd2cf7
Locolm <clement.hl34@gmail.com>
2025-01-20 16:56:06 +0100

    fix position location move-to

--- llm.py
+++ llm.py
@@ -82,7 +82,7 @@ def completion(prompt):
                 
                 locations_data = [context.load_json(f"./locations/locations_{matching_location}.json") for matching_location in locations_context]             
                 
-                prompt = """Créer """ + util.extract_last_part(prompt) + """ Donne un nom, une description, une position (x, y en entiers), une liste d'objets (avec nom, description et prix), et une liste de monstres (uniquement si le donjon est de type hostile). Les monstres doivent inclure nom, description, puissance, etat, nombre, et objets. Répondez sous la forme d'un JSON structuré contenant uniquement les champs suivants : nom, description, type(boutique, donjon, sauvage, confort) position, objets, et monstres."""
+                prompt = """Créer """ + util.extract_last_part(prompt) + """ Donne un nom, une description, une position (x"""+str(x)+""", y"""+str(y)+""" en entiers), une liste d'objets (avec nom, description et prix), et une liste de monstres (uniquement si le donjon est de type hostile). Les monstres doivent inclure nom, description, puissance, etat, nombre, et objets. Répondez sous la forme d'un JSON structuré contenant uniquement les champs suivants : nom, description, type(boutique, donjon, sauvage, confort) position, objets, et monstres."""
                 
                 prompt =    f"Contexte des lieux alentours : {locations_data}\n"\
                             f"Prompt : " + prompt.strip() + "\n"
